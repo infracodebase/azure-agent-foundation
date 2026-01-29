51173c988eb68ed5b9627c755171991ce915ebbf
Justin O'Connor <jsoconno@gmail.com>
2026-01-29 03:19:03 +0000

    Remove AI Hub managed identity storage account role assignment from Terraform configuration

--- terraform/foundry.tf
+++ terraform/foundry.tf
@@ -18,12 +18,6 @@ resource "azurerm_cognitive_account" "ai_foundry" {
   tags = local.common_tags
 }
 
-# Grant AI Hub managed identity access to storage account
-resource "azurerm_role_assignment" "ai_hub_storage_access" {
-  scope                = azurerm_storage_account.this.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_cognitive_account.ai_foundry.identity[0].principal_id
-}
 
 # Grant AI Hub managed identity access to Key Vault
 resource "azurerm_role_assignment" "ai_hub_kv_access" {
